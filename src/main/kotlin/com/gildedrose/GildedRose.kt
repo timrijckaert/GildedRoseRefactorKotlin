0095e464b433efa1ea608401fc473678bc05bb76
timrijckaert <tim.rijckaert@gmail.com>
2022-07-16 18:54:11 +0200

    Clean-up: Use destructuring to make a bit more readable

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -17,9 +17,9 @@ class GildedRose(var items: Array<Item>) {
             val currSellIn = SellIn(it.sellIn)
             val currQuality = Quality(it.quality)
 
-            val itemStrategy = strategyMap.getOrDefault(it.name, defaultItemStrategy)
-            val newSellIn = itemStrategy.first.updateSellInValue(currSellIn)
-            val newQuality = itemStrategy.second.updateQuality(currSellIn, currQuality)
+            val (sellInStrategy, qualityStrategy) = strategyMap.getOrDefault(it.name, defaultItemStrategy)
+            val newSellIn = sellInStrategy.updateSellInValue(currSellIn)
+            val newQuality = qualityStrategy.updateQuality(currSellIn, currQuality)
 
             it.quality = (currQuality + newQuality).quality
             it.sellIn = newSellIn.days
