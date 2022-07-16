56cdbcc85f4c5e994bd6fab363fdc771a5fb9ff3
timrijckaert <tim.rijckaert@gmail.com>
2022-07-16 14:54:21 +0200

    Convert TexttestFixture to tests

---
+++ src/main/kotlin/com/gildedrose/model.kt
@@ -0,0 +1,7 @@
+package com.gildedrose
+
+@JvmInline
+value class SellIn(val days: Int)
+
+@JvmInline
+value class Quality(val quality: UInt)
--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,15 +1,135 @@
 package com.gildedrose
 
 import io.kotest.core.spec.style.StringSpec
-import org.junit.jupiter.api.Assertions.assertEquals
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
+import io.kotest.matchers.equality.shouldBeEqualToComparingFields
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.next
 
 internal class GildedRoseTest : StringSpec() {
     init {
-        "foo" {
-            val items = arrayOf(Item("foo", 0, 0))
-            val app = GildedRose(items)
-            app.updateQuality()
-            assertEquals("foo", app.items[0].name)
+        "+5 Dexterity Vest" {
+            val dexterityVest = "+5 Dexterity Vest"
+            table(
+                headers("days passed", "expected sell in", "expected quality"),
+                row(1, 9, 19),
+                row(9, 1, 11),
+                row(10, 0, 10),
+                row(11, -1, 8),
+                row(15, -5, 0),
+                row(16, -6, 0),
+            ).forAll { days, sellIn, quality ->
+                val item = Item(dexterityVest, 10, 20)
+                val gildedRose = GildedRose(arrayOf(item))
+                repeat(days) { gildedRose.updateQuality() }
+                item shouldBeEqualToComparingFields Item(dexterityVest, sellIn, quality)
+            }
+        }
+
+        "Aged Brie" {
+            val agedBrie = "Aged Brie"
+            table(
+                headers("days passed", "expected sell in", "expected quality"),
+                row(1, 1, 1),
+                row(2, 0, 2),
+                row(3, -1, 4),
+                row(4, -2, 6),
+                row(5, -3, 8),
+                row(26, -24, 50),
+                row(27, -25, 50),
+            ).forAll { days, sellIn, quality ->
+                val item = Item(agedBrie, 2, 0)
+                val gildedRose = GildedRose(arrayOf(item))
+                repeat(days) { gildedRose.updateQuality() }
+                item shouldBeEqualToComparingFields Item(agedBrie, sellIn, quality)
+            }
+        }
+
+        "Elixir of the Mongoose" {
+            val elixirOfTheMongoose = "Elixir of the Mongoose"
+            table(
+                headers("days passed", "expected sell in", "expected quality"),
+                row(1, 4, 6),
+                row(2, 3, 5),
+                row(5, 0, 2),
+                row(6, -1, 0),
+                row(7, -2, 0),
+            ).forAll { days, sellIn, quality ->
+                val item = Item(elixirOfTheMongoose, 5, 7)
+                val gildedRose = GildedRose(arrayOf(item))
+                repeat(days) { gildedRose.updateQuality() }
+                item shouldBeEqualToComparingFields Item(elixirOfTheMongoose, sellIn, quality)
+            }
+        }
+
+        "Sulfuras, Hand of Ragnaros" {
+            val sulfuras = "Sulfuras, Hand of Ragnaros"
+            val sellIn = Arb.int().next()
+            val quality = Arb.int().next()
+            val item = Item(sulfuras, sellIn, quality)
+            val gildedRose = GildedRose(arrayOf(item))
+            repeat(100) { gildedRose.updateQuality() }
+            item shouldBeEqualToComparingFields Item(sulfuras, sellIn, quality)
+        }
+
+        "Backstage passes to a TAFKAL80ETC concert" {
+            val backstagePass = "Backstage passes to a TAFKAL80ETC concert"
+            table(
+                headers("days passed", "expected sell in", "expected quality"),
+                row(1, 14, 21),
+                row(2, 13, 22),
+                row(5, 10, 25),
+                row(6, 9, 27),
+                row(10, 5, 35),
+                row(11, 4, 38),
+                row(14, 1, 47),
+                row(15, 0, 50),
+                row(16, -1, 0),
+                row(17, -2, 0),
+            ).forAll { days, sellIn, quality ->
+                val item = Item(backstagePass, 15, 20)
+                val gildedRose = GildedRose(arrayOf(item))
+                repeat(days) { gildedRose.updateQuality() }
+                item shouldBeEqualToComparingFields Item(backstagePass, sellIn, quality)
+            }
+        }
+
+        "Conjured Mana Cake" {
+            val conjuredManaCake = "Conjured Mana Cake"
+            table(
+                headers("days passed", "expected sell in", "expected quality"),
+                row(1, 2, 8),
+                row(2, 1, 6),
+                row(3, 0, 4),
+                row(4, -1, 0),
+                row(5, -2, 0),
+            ).forAll { days, sellIn, quality ->
+                val item = Item(conjuredManaCake, 3, 10)
+                val gildedRose = GildedRose(arrayOf(item))
+                repeat(days) { gildedRose.updateQuality() }
+                item shouldBeEqualToComparingFields Item(conjuredManaCake, sellIn, quality)
+            }
+        }
+
+        "Normal item" {
+            val unknown = "normal item"
+            table(
+                headers("days passed", "expected sell in", "expected quality"),
+                row(1, 2, 9),
+                row(2, 1, 8),
+                row(3, 0, 7),
+                row(4, -1, 5),
+                row(5, -2, 3),
+            ).forAll { days, sellIn, quality ->
+                val item = Item(unknown, 3, 10)
+                val gildedRose = GildedRose(arrayOf(item))
+                repeat(days) { gildedRose.updateQuality() }
+                item shouldBeEqualToComparingFields Item(unknown, sellIn, quality)
+            }
         }
     }
 }
